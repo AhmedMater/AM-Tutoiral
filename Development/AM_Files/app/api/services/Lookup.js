b0dc871acf52257a3ff0a965d7adf9e347945d4e
AhmedMater <ahmedmotair@gmail.com>
2016-11-22 05:45:29 +0200

    j

--- Development/AM_Files/app/api/services/Lookup.js
+++ Development/AM_Files/app/api/services/Lookup.js
@@ -5,4 +5,16 @@
 var config = require('../../configuration');
 var SystemParameters = require(config.Common.SystemParameters);
 
-var exports = module.exports = {};
+var exports = module.exports = {
+    getAllCourseLevels: function(){
+        return SystemParameters.CourseLevel;
+    },
+
+    getAllCourseTypes: function(){
+        return SystemParameters.CourseType;
+    },
+
+    getAllReferenceTypes: function(){
+        return SystemParameters.ReferenceType;
+    }
+};
--- Development/AM_Files/app/main/admin/course/addNewCourse/addNewCourse_get.js
+++ Development/AM_Files/app/main/admin/course/addNewCourse/addNewCourse_get.js
@@ -3,11 +3,15 @@
  */
 var exports = module.exports = {};
 var config = require('../../../../configuration');
+var lookupService = require('../../../' + config.Services.Lookup);
 
 exports.go = function(req,res) {
     // we have to validate if the user is admin
 
     res.render(config.Views.addNewCourse, {
-        title: "Add New Course"
+        title: "Add New Course",
+        courseLevel: lookupService.getAllCourseLevels(),
+        courseType: lookupService.getAllCourseTypes(),
+        referenceType:lookupService.getAllReferenceTypes()
     });
 };
